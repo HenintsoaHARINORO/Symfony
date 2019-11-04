a54e921f724b81bde121a3c3a514951bedfb2775
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2019-11-04 16:32:39 +0300

    installing twig

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
