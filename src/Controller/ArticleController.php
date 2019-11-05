50b340a5f7a755f40d4d706d7b0e78c120032dff
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2019-11-05 12:04:54 +0300

    using twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,17 +5,29 @@
  * Date: 11/4/19
  * Time: 3:23 PM
  */
+
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/news/{slug}")
      */
 public function show($slug)
-{
-    return new Response(sprintf('OMG MY SYMFONY FIRST PAGE  : "%s"',$slug));
+{   $comments = [
+    'bye',
+    'OOH I  did not notice this is an array',
+];
+
+
+    return $this->render('article/show.html.twig',[
+        'title'=>$slug,
+        'comments'=>$comments,
+
+    ]);
+
 
 }
 }
