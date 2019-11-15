1e490afdba5c313c6ca960afc7aa42f4caa0118d
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2019-11-15 15:36:03 +0300

    adding javascript

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,13 +7,21 @@
  */
 
 namespace App\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/news/{slug}")
+     * @Route("/",name="app_homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('article/homepage.html.twig');
+    }
+    /**
+     * @Route("/news/{slug}",name="article_show")
      */
 public function show($slug)
 {   $comments = [
